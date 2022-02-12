d3c4b7cf83e3e7a514d51f8a21257b6f0aeb2d0f
littlebuffett <docdoca@naver.com>
2022-02-12 10:34:46 +0900

    회원가입 실패 원인에 따라 Toast 출력 (닉네임이 빈칸일 경우,이메일이 빈칸일 경우, 이메일이 올바른 형식이 아닐 경우, 비밀번호가 빈칸일 경우, 비밀번호 확인이 일치하지 않을 경우, 인증코드가 일치하지 않을 경우)

--- app/src/main/java/com/inu/appcenter/inuit/SingUpActivity.kt
+++ app/src/main/java/com/inu/appcenter/inuit/SingUpActivity.kt
@@ -4,14 +4,10 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
 import androidx.activity.viewModels
-import com.inu.appcenter.inuit.retrofit.Email
-import com.inu.appcenter.viewmodel.ClubListViewModel
 import com.inu.appcenter.viewmodel.SignUpViewModel
 
 class SingUpActivity : AppCompatActivity() {
@@ -37,40 +33,16 @@ class SingUpActivity : AppCompatActivity() {
         passwordCheck = findViewById(R.id.et_password_check)
 
         val backButton = findViewById<ImageButton>(R.id.btn_back)
-        backButton.setOnClickListener {
-            finish()
-        }
+        backButton.setOnClickListener { finish() }
 
-        val sendCode = findViewById<TextView>(R.id.tv_send_certification)
-        sendCode.setOnClickListener {
-            outFocusEditText()
-
-            if (!isCorrectEmail()) {
-                // 이메일이 올바른 형식이 아닙니다.
-                showToastMsg(getString(R.string.msg_wrong_email))
-
-            }else{
-                viewModel.postEmail(email.text.toString())
-                //입력하신 이메일로 인증번호가 전송되었습니다.
-                showToastMsg(getString(R.string.msg_code_send))
-            }
+        val sendCertification = findViewById<TextView>(R.id.tv_send_certification)
+        sendCertification.setOnClickListener {
+            outFocusEditText() //키보드 내리기
+            sendCode()
         }
 
         val signUpButton = findViewById<Button>(R.id.btn_singup)
-        signUpButton.setOnClickListener {
-
-            if(password.text.toString() != passwordCheck.text.toString()) {
-                //비밀번호가 일치하지 않습니다!
-                showToastMsg(getString(R.string.msg_incorrect_password))
-            }else{
-                if (viewModel.isVerifiedEmail(email.text.toString(),certificationNum.text.toString())){
-                    // 회원가입 성공, 닉네임, 이메일, 패스워드 데이터 전송
-                } else{
-                    //인증번호가 일치하지 않습니다.
-                    showToastMsg(getString(R.string.msg_wrong_code))
-                }
-            }
-        }
+        signUpButton.setOnClickListener { signUp() }
     }
 
     companion object {
@@ -79,19 +51,57 @@ class SingUpActivity : AppCompatActivity() {
         }
     }
 
-    fun outFocusEditText(){
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(email.windowToken, 0)
-    }
+    private fun isEmptyNickName(): Boolean = nickname.text.isEmpty()
 
-    fun isCorrectEmail():Boolean{
-        var inputEmail = email.text.toString()
-        var divEmail = inputEmail.split("@")
-        Log.d("email","${divEmail[1]}")
+    private fun isEmptyEmail(): Boolean = email.text.isEmpty()
+
+    private fun isCorrectEmail(): Boolean{
+        val inputEmail = email.text.toString()
+        if(!inputEmail.contains('@')){ return false }
+
+        val divEmail = inputEmail.split("@")
+        Log.d("email", divEmail[1])
         return divEmail[1] == correct
     }
 
-    fun showToastMsg(msg:String){
-        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
+    private fun isEmptyPassword(): Boolean = password.text.isEmpty()
+
+    private fun isCorrectPassword(): Boolean = password.text.toString() == passwordCheck.text.toString()
+
+    private fun sendCode(){
+        if (isCorrectEmail()) {
+            viewModel.postEmail(email.text.toString())
+            showToastMsg(getString(R.string.msg_code_send)) //입력하신 이메일로 인증번호가 전송되었습니다.
+        }else{
+            showToastMsg(getString(R.string.msg_wrong_email)) // 올바르지 않은 이메일 주소입니다.
+        }
+    }
+
+    private fun signUp(){
+        if(isEmptyNickName()){
+            showToastMsg(getString(R.string.msg_empty_nickname)) //닉네임을 입력해주세요.
+        }else if(isEmptyEmail()){
+            showToastMsg(getString(R.string.msg_empty_email)) //이메일을 입력해주세요.
+        }else if(!isCorrectEmail()){
+            showToastMsg(getString(R.string.msg_wrong_email)) // 올바르지 않은 이메일 주소입니다.
+        }else if(isEmptyPassword()){
+            showToastMsg(getString(R.string.msg_empty_password))
+        }
+        else if(!isCorrectPassword()) {
+            showToastMsg(getString(R.string.msg_incorrect_password)) //비밀번호가 일치하지 않습니다!
+        }else{
+            if (viewModel.isVerifiedEmail(email.text.toString(),certificationNum.text.toString())){
+                // 회원가입 성공, 닉네임, 이메일, 패스워드 데이터 전송
+            } else{
+                showToastMsg(getString(R.string.msg_wrong_code)) //인증번호가 일치하지 않습니다.
+            }
+        }
+    }
+
+    fun showToastMsg(msg:String){ Toast.makeText(this,msg,Toast.LENGTH_SHORT).show() }
+
+    private fun outFocusEditText(){
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(email.windowToken, 0)
     }
 }
