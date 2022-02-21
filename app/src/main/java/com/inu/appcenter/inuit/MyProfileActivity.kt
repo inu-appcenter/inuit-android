4f673f1173cf804aeaea5e795f89c8cd16cdc291
littlebuffett <docdoca@naver.com>
2022-02-21 13:11:18 +0900

    MyClubListAdapter 분리

--- app/src/main/java/com/inu/appcenter/inuit/MyProfileActivity.kt
+++ app/src/main/java/com/inu/appcenter/inuit/MyProfileActivity.kt
@@ -11,6 +11,7 @@ import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.inu.appcenter.inuit.recycler.MyClubListAdapter
 import com.inu.appcenter.inuit.recycler.item.ClubItem
 import com.inu.appcenter.inuit.retrofit.dto.Circle
 
@@ -32,7 +33,7 @@ class MyProfileActivity : AppCompatActivity() {
 
         val recycler_myclub_List = findViewById<RecyclerView>(R.id.recycler_myclub_list)
         recycler_myclub_List.layoutManager = LinearLayoutManager(this)
-        val adapter = MyClubsAdapter()
+        val adapter = MyClubListAdapter()
         recycler_myclub_List.adapter = adapter
         adapter.setSampleData()
     }
@@ -40,65 +41,6 @@ class MyProfileActivity : AppCompatActivity() {
     companion object {
         fun newIntent(context: Context): Intent {
             return Intent(context, MyProfileActivity::class.java)
-        }
-    }
-
-    class MyClubViewHolder(view: View): RecyclerView.ViewHolder(view), View.OnClickListener{
-
-        var id : Int? = null
-        val clubName = view.findViewById<TextView>(R.id.tv_recycler_my_club_list)
-
-        init {
-            clubName.setOnClickListener(this)
-        }
-
-        fun bind(data:ClubItem){
-            id = data.id
-            clubName.text = data.name
-        }
-
-        override fun onClick(v: View?) {
-            //해당 동아리 상세 페이지로 이동.
-        }
-    }
-
-    class MyClubsAdapter() : RecyclerView.Adapter<MyClubViewHolder>(){
-
-        private val items = mutableListOf<ClubItem>()
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyClubViewHolder {
-            val inflater = LayoutInflater.from(parent.context)
-            val view = inflater.inflate(R.layout.recycler_my_club_list_item, parent, false)
-            return MyClubViewHolder(view)
-        }
-
-        override fun onBindViewHolder(holder: MyClubViewHolder, position: Int) {
-            val data = items[position]
-            holder.bind(data)
-        }
-
-        override fun getItemCount(): Int = items.size
-
-        fun setSampleData(){
-            addItem(ClubItem(1,"인유공방"))
-            addItem(ClubItem(1,"인유공방"))
-            addItem(ClubItem(1,"인유공방"))
-            addItem(ClubItem(1,"인유공방"))
-            addItem(ClubItem(1,"인유공방"))
-            addItem(ClubItem(1,"인유공방"))
-            addItem(ClubItem(1,"인유공방"))
-        }
-
-        fun addItem(item: ClubItem) {
-            this.items.add(item)
-        }
-
-        fun addListToItems(list:List<Circle>?){ //List<Circle>은 뷰모델로부터 받는다.
-            items.clear()
-            list?.forEach {
-                addItem(ClubItem(it.id,it.name))
-            }
-            notifyDataSetChanged()
         }
     }
 }
---
+++ app/src/main/java/com/inu/appcenter/inuit/recycler/MyClubListAdapter.kt
@@ -0,0 +1,65 @@
+package com.inu.appcenter.inuit.recycler
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.inu.appcenter.inuit.R
+import com.inu.appcenter.inuit.recycler.item.ClubItem
+import com.inu.appcenter.inuit.retrofit.dto.Circle
+
+class MyClubListAdapter() : RecyclerView.Adapter<MyClubListAdapter.MyClubViewHolder>() {
+
+    private val items = mutableListOf<ClubItem>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyClubViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.recycler_my_club_list_item, parent, false)
+        return MyClubViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder:MyClubViewHolder, position: Int) {
+        val data = items[position]
+        holder.bind(data)
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    fun setSampleData(){
+        addItem(ClubItem(1,"인유공방"))
+        addItem(ClubItem(1,"인유공방"))
+        addItem(ClubItem(1,"인유공방"))
+        addItem(ClubItem(1,"인유공방"))
+        addItem(ClubItem(1,"인유공방"))
+        addItem(ClubItem(1,"인유공방"))
+        addItem(ClubItem(1,"인유공방"))
+    }
+
+    fun addItem(item: ClubItem) {
+        this.items.add(item)
+    }
+
+    fun addListToItems(list:List<Circle>?){ //List<Circle>은 뷰모델로부터 받는다.
+        items.clear()
+        list?.forEach {
+            addItem(ClubItem(it.id,it.name))
+        }
+        notifyDataSetChanged()
+    }
+
+    class MyClubViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+        var id : Int? = null
+        val clubName = view.findViewById<TextView>(R.id.tv_recycler_my_club_list)
+
+        fun bind(data:ClubItem){
+            id = data.id
+            clubName.text = data.name
+            itemView.setOnClickListener {
+                Toast.makeText(itemView.context,"Selected id = $id", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
