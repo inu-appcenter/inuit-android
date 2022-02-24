8428b0b302adf0b1db3659b794d3c79cf43775ea
littlebuffett <docdoca@naver.com>
2022-02-24 14:28:39 +0900

    memberInfo 데이터 참조하여 EditProfileActivity UI 세팅

--- app/src/main/java/com/inu/appcenter/inuit/EditProfileActivity.kt
+++ app/src/main/java/com/inu/appcenter/inuit/EditProfileActivity.kt
@@ -6,9 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.TextView
+import com.inu.appcenter.inuit.login.App
 
 class EditProfileActivity : AppCompatActivity() {
 
+    lateinit var userEmail: TextView
     lateinit var newNickname : EditText
     lateinit var newPassword : EditText
     lateinit var newPasswordCheck : EditText
@@ -17,10 +20,15 @@ class EditProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
 
+        val memberInfo = App.memberInfo
+
+        userEmail = findViewById(R.id.tv_user_email_editprofile)
         newNickname = findViewById(R.id.et_new_nickname)
         newPassword = findViewById(R.id.et_new_password)
         newPasswordCheck = findViewById(R.id.et_new_password_check)
 
+        userEmail.text = memberInfo?.email
+        newNickname.setText(memberInfo?.nickname)
         val cancelButton = findViewById<ImageButton>(R.id.btn_cancel)
         cancelButton.setOnClickListener {
             finish()
