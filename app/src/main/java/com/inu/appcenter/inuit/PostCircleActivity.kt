4fc7d4ab56ddba3bf1011b8b6f1d76a10f654be5
littlebuffett <docdoca@naver.com>
2022-02-28 16:41:47 +0900

    모집기간 선택 dateRangePicker, 팝업 메뉴 구현(직접선택, 상시모집, 모집 마감)

--- app/src/main/java/com/inu/appcenter/inuit/PostCircleActivity.kt
+++ app/src/main/java/com/inu/appcenter/inuit/PostCircleActivity.kt
@@ -2,19 +2,23 @@ package com.inu.appcenter.inuit
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
-import android.widget.ImageButton
-import android.widget.TextView
-import android.widget.Toast
+import android.util.Log
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.esafirm.imagepicker.features.ImagePickerConfig
 import com.esafirm.imagepicker.features.ImagePickerSavePath
 import com.esafirm.imagepicker.features.registerImagePicker
 import com.esafirm.imagepicker.model.Image
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.inu.appcenter.inuit.recycler.ImagePreviewAdapter
+import java.text.SimpleDateFormat
+import java.util.*
 
 class PostCircleActivity : AppCompatActivity() {
 
@@ -29,16 +33,29 @@ class PostCircleActivity : AppCompatActivity() {
     private lateinit var posterImageAdapter : ImagePreviewAdapter
     private lateinit var addPosterImage : TextView
 
+    private lateinit var setRecruitSchedule : TextView
+    private lateinit var dropDownRecruitSchedule : ImageButton
+
+    private lateinit var applyLink:TextView
+    private lateinit var applyConstraint : ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_circle)
 
         profileRecyclerView = findViewById(R.id.recycler_profile_image)
+        addProfileImage = findViewById(R.id.tv_post_circle_profile_image)
+        posterRecyclerView = findViewById(R.id.recycler_poster_image)
+        addPosterImage = findViewById(R.id.tv_post_circle_poster_image)
+        setRecruitSchedule = findViewById(R.id.tv_post_circle_recruit_schedule)
+        dropDownRecruitSchedule = findViewById(R.id.ibtn_recruit_schedule)
+        applyLink = findViewById(R.id.tv_title_circle_apply)
+        applyConstraint = findViewById(R.id.et_post_apply_group)
+
         profileRecyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
         profileImageAdapter = ImagePreviewAdapter()
         profileRecyclerView.adapter = profileImageAdapter
 
-        addProfileImage = findViewById(R.id.tv_post_circle_profile_image)
         addProfileImage.setOnClickListener {
             if(profileImageAdapter.itemsSize()==0){
                 profilePickerLauncher.launch(profilePickerConfig)
@@ -47,18 +64,25 @@ class PostCircleActivity : AppCompatActivity() {
             }
         }
 
-        posterRecyclerView = findViewById(R.id.recycler_poster_image)
         posterRecyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
         posterImageAdapter = ImagePreviewAdapter()
         posterRecyclerView.adapter = posterImageAdapter
 
-        addPosterImage = findViewById(R.id.tv_post_circle_poster_image)
+
         addPosterImage.setOnClickListener {
             if(posterImageAdapter.itemsSize() == 0){
                 posterPickerLauncher.launch(posterPickerConfig)
             }else{
                 showToastMsg(getString(R.string.post_poster_image_msg))
             }
+        }
+
+        setRecruitSchedule.setOnClickListener {
+            pickDateRange()
+        }
+
+        dropDownRecruitSchedule.setOnClickListener {
+            dropDownMenu()
         }
 
         val backButton = findViewById<ImageButton>(R.id.btn_cancel_post_circle)
@@ -73,12 +97,14 @@ class PostCircleActivity : AppCompatActivity() {
         }
     }
 
+    // -- 동아리 프로필 이미지피커 설정 --
     private val profilePickerConfig = ImagePickerConfig{
         savePath = ImagePickerSavePath("Camera")
         savePath = ImagePickerSavePath(Environment.getExternalStorageDirectory().path, isRelative = false)
         limit = 1
     }
 
+    // -- 동아리 프로필 이미지피커 런쳐 --
     private val profilePickerLauncher = registerImagePicker { result: List<Image> ->
 
         result.forEach { image ->
@@ -90,12 +116,14 @@ class PostCircleActivity : AppCompatActivity() {
         }
     }
 
+    // -- 동아리 포스터 이미지피커 설정 --
     private val posterPickerConfig = ImagePickerConfig{
         savePath = ImagePickerSavePath("Camera")
         savePath = ImagePickerSavePath(Environment.getExternalStorageDirectory().path, isRelative = false)
         limit = 1
     }
 
+    // -- 동아리 포스터 이미지피커 런쳐 --
     private val posterPickerLauncher = registerImagePicker { result: List<Image> ->
 
         result.forEach { image ->
@@ -105,6 +133,35 @@ class PostCircleActivity : AppCompatActivity() {
             posterImage.clear()
             posterImage.addAll(result)
         }
+    }
+
+    private fun pickDateRange(){
+        val dateRangePicker =
+            MaterialDatePicker.Builder.dateRangePicker()
+                .setTitleText(getString(R.string.post_circle_recruit_schedule_dialog_msg))
+                .build()
+        dateRangePicker.addOnNegativeButtonClickListener{ dateRangePicker.dismiss() }
+        dateRangePicker.addOnPositiveButtonClickListener {
+            val startDate = SimpleDateFormat("yyyy.MM.dd", Locale.getDefault()).format(it.first)
+            val endDate = SimpleDateFormat("yyyy.MM.dd", Locale.getDefault()).format(it.second)
+            setRecruitSchedule.text = startDate + " ~ " + endDate
+            Log.d("test", "startDate: $startDate, endDate : $endDate")
+        }
+        dateRangePicker.show(supportFragmentManager,dateRangePicker.toString())
+    }
+
+    private fun dropDownMenu(){
+        val pop = PopupMenu(this, dropDownRecruitSchedule)
+        menuInflater.inflate(R.menu.menu_recruit_schedule, pop.menu)
+        pop.setOnMenuItemClickListener {
+            when(it.itemId){
+                R.id.menu_select_day -> pickDateRange()
+                R.id.menu_all_day_open -> setRecruitSchedule.text = it.title
+                R.id.menu_close -> setRecruitSchedule.text = it.title
+            }
+            false
+        }
+        pop.show()
     }
 
     fun showToastMsg(msg:String){ Toast.makeText(this,msg, Toast.LENGTH_SHORT).show() }
